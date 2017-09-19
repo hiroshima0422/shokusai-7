64cfc552b196146d74f30df876f94a662f4baf32
hiro <hiroshima6504@gmail.com>
2017-09-19 11:59:47 +0000

    post

--- database/migrations/2017_09_18_185652_create_microposts_table.php
+++ database/migrations/2017_09_18_185652_create_microposts_table.php
@@ -12,7 +12,7 @@ class CreateMicropostsTable extends Migration
      */
     public function up()
     {
-         Schema::create('microposts', function (Blueprint $table) {
+        Schema::create('microposts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->string('content');
